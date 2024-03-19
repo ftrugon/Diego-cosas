dbc530ae12a7c33dce3238981d0319e201e78336
franconil <franconil04@gmail.com>
2024-03-19 01:07:43 +0100

    compartir brihin

---
+++ Assets/Jugador.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public float speed = 5f; // Velocidad de movimiento del jugador
+    private GameObject circulo;
+
+    void Start(){
+        circulo = GameObject.Find("Circle");
+    }
+
+    void Update()
+    {
+
+        // Obtener la entrada del usuario para moverse horizontal y verticalmente
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calcula el desplazamiento en función de la entrada y la velocidad
+        float horizontalMovement = moveHorizontal * speed * Time.deltaTime;
+        float verticalMovement = moveVertical * speed * Time.deltaTime;
+
+        // Mueve el objeto en el plano horizontal y vertical (en 2D)
+        transform.Translate(new Vector2(horizontalMovement, verticalMovement));
+        
+    }
+
+}
